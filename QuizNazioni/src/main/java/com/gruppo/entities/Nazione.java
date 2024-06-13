3459dae9ae44b8d69da02f51930884bce441be18
Alessandro Bruzzese <69127023+Bruzzii@users.noreply.github.com>
2024-06-13 15:25:24 +0200

    Aggiunto ID a nazione

--- QuizNazioni/src/main/java/com/gruppo/entities/Nazione.java
+++ QuizNazioni/src/main/java/com/gruppo/entities/Nazione.java
@@ -2,6 +2,8 @@ package com.gruppo.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,6 +12,9 @@ import jakarta.persistence.Table;
 public class Nazione {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
 	@Column(length = 2)
 	private String alphacod;
 	
