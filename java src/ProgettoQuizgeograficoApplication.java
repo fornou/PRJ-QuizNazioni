8109f0f77dde377d48aebbf807faa078562c8d82
fornou <mattiaforneron@gmail.com>
2024-06-13 10:47:45 +0200

    Aggiornamento Cartelle Necessarie
    Eliminazione della cartella zip, ognuno si tiene le sources

---
+++ java src/ProgettoQuizgeograficoApplication.java
@@ -0,0 +1,13 @@
+package com.prette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoQuizgeograficoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoQuizgeograficoApplication.class, args);
+	}
+
+}
